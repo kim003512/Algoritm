c3fa4204d7f5a6ff03212ba8ad4feae005490b3e
kim003512@naver.com <kim003512@naver.com>
2022-04-05 15:06:19 +0900

    add 영역구하기

--- src/BaekJoon/dfs/영역구하기.java
+++ src/BaekJoon/dfs/영역구하기.java
@@ -1,4 +1,28 @@
 package BaekJoon.dfs;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class 영역구하기 {
+    static int map[][];
+    static boolean visited[][];
+    static int M, N, K;
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+
+        map = new int[N][M];
+        visited = new boolean[N][M];
+
+    }
+    static void dfs(){
+
+    }
 }
