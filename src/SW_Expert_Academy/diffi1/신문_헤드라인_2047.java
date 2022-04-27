41048f276800377b749b3b8f844572f6e100e20a
kim003512@naver.com <kim003512@naver.com>
2022-04-27 10:19:45 +0900

    add 신문 헤드라인

---
+++ src/SW_Expert_Academy/diffi1/신문_헤드라인_2047.java
@@ -0,0 +1,15 @@
+package SW_Expert_Academy.diffi1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 신문_헤드라인_2047 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = (new BufferedReader(new InputStreamReader(System.in)));
+
+        String str = br.readLine();
+
+        System.out.println(str.toUpperCase());
+    }
+}
