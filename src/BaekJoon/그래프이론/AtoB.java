935045793812c729b6401e5c47e1ce36f433d1dd
kim003512@naver.com <kim003512@naver.com>
2022-05-06 14:04:45 +0900

    add A→B

---
+++ src/BaekJoon/그래프이론/AtoB.java
@@ -0,0 +1,33 @@
+package BaekJoon.그래프이론;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class AtoB {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+
+        int cnt=1;
+
+        while(A != B){
+            if(B<A){
+                System.out.println("-1");
+                break;
+            }
+            if(B % 10 == 1) B /= 10;
+            else if(B % 2 == 0) B /= 2;
+            else {
+                System.out.println(-1);
+                break;
+            }
+            cnt++;
+        }
+        System.out.println(cnt);
+    }
+}
